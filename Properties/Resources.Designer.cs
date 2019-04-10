a04057ac1ebb082643591851fbabda48f59fb03b
Yordanka Buhalova <danito1@live.com>
2019-04-10 21:33:50 +0200

    clean code

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace WindowsFormsAppProjekt.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap download {
-            get {
-                object obj = ResourceManager.GetObject("download", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap preview_62928VWhv2ri94T_0012 {
             get {
                 object obj = ResourceManager.GetObject("preview-62928VWhv2ri94T_0012", resourceCulture);
